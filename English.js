2895f0a5b258f600193d621b95d3b13e1aa8566a
Cambysses <selxxa@gmail.com>
2020-06-16 21:54:42 -0300

    Create English.js

---
+++ English.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name     MyAnimeList English Titles
+// @version  1
+// @include http*://myanimelist.net/topanime.php*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js 
+// @grant    none
+// @run-at document-end
+// ==/UserScript==
+
+// Gets list of URLs for all animes on page.
+const animes = document.getElementsByClassName("detail");
+
+// Runs query for every title and appends English name if it exists.
+for(i = 0; i < animes.length; i++)
+{
+    var animeDetail = animes[i].getElementsByClassName("di-ib clearfix");
+    var animeID = animeDetail[0].firstChild.id;
+    var animeURL = animeDetail[0].firstChild.href;
+    jQuery(animeID).load(animeURL + ' .title-english'); 
+}
